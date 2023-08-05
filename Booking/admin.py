865b4af251d80ec51ee171b800685c4528576fbe
salms358 <sa083850@gmail.com>
2023-08-05 13:45:09 +0000

    creating the templates folder and adding to the admins and models.py files

--- Booking/admin.py
+++ Booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# Import Booking model from models.py
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'email','phone_number','date','table_count','cancelled')
+    search_fields = ('customer_name','venue')
+    list_filter = ('venue', 'phone_number')
+
+
--- Booking/models.py
+++ Booking/models.py
@@ -2,19 +2,22 @@
 from django.db import models
 from .venue import Venue
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+
 class Booking(models.Model):
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Location, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
-    date_time = models.DateTimeField()
+    date = models.DateTimeField()
     table_count = models.PositiveIntegerField(default=1)
-    is_canceled = models.BooleanField(default=False)
+    cancelled = models.BooleanField(default=False)
 
-class Menu(models.Model):
-    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
-    appetizer = models.CharField(max_length=100)
-    main_course = models.CharField(max_length=100)
-    dessert = models.CharField(max_length=100)
+
 
 
