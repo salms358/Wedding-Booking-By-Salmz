83f13a1cee9059dfcb3c94c1292e49bff51b5668
salms358 <sa083850@gmail.com>
2023-08-09 14:06:22 +0000

    Added more HTML files for editing, deleting, viewing bookings added CRUD functionality to my views.py

--- Booking/views.py
+++ Booking/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.db import IntegrityError
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.forms import UserCreationForm
 from .forms import BookingForm
@@ -45,15 +45,18 @@ class CreateProfile(View):
         CreateUserProfile.save()
 
         return redirect(reverse('home'))
-
+        
+def register(request):
+    return render(request,'register.html')
+    
 def index_view(request):
     return render(request, 'base.html')
 
 def about_us(request):
     return render(request, 'about_us.html')
-def LoginView(request):
+def account_login(request):
     return render(request, 'login.html')
-
+# CRUD functionality
 def create_booking(request):
     if request.method == 'POST':
         booking_form = BookingForm(request.POST)
@@ -65,3 +68,25 @@ def create_booking(request):
 
     return render(request, 'create_booking.html', {'booking_form': booking_form})
 
+def view_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    return render(request, 'view_booking.html', {'booking': booking})
+
+def update_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        booking_form = BookingForm(request.POST, instance=booking)
+        if booking_form.is_valid():
+            booking_form.save()
+            return redirect('view_booking', booking_id=booking.id)
+        else:
+            booking_form = BookingForm(instance=booking)
+    
+    return render(request, 'update_booking.html', {'booking_form': booking_form})
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('index')
+    
+    return render(request, 'delete_booking.html', {'booking': booking})
--- WeddingBookingz/settings.py
+++ WeddingBookingz/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -48,9 +49,11 @@ INSTALLED_APPS = [
     'Booking',
 ]
 SITE_ID = 1
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
