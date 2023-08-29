19a7737762bc596035a0b6df007d77edb070270e
salms358 <sa083850@gmail.com>
2023-08-29 18:53:02 +0000

    another attempt to get the url to work

--- WeddingBookingz/settings.py
+++ WeddingBookingz/settings.py
@@ -26,6 +26,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+CLOUDINARY_STORAGE [
+    
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET')
+]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -50,6 +56,7 @@ INSTALLED_APPS = [
     'cloudinary_storage'
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms'
     'Booking',
 ]
 SITE_ID = 1
@@ -149,14 +156,4 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'your-email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
-EMAIL_VERIFICATION = 'optional'
-ACCOUNT_EMAIL_VERIFICATION = 'optional'
 
